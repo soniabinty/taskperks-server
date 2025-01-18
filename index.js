2daf98fb56c37cd70acecceafd079d68eae75132
Sonia Binty <sonianazninbinty@gmail.com>
2025-01-19 00:34:45 +0600

    added deletation function of task

--- index.js
+++ index.js
@@ -89,6 +89,16 @@ async function run() {
   res.send(result);
   
     });
+
+
+app.delete('/alltasks/:id', async(req , res) =>{
+  const id = req.params.id
+  const query ={ _id : new ObjectId(id)}
+
+  const result = await taskCollection.deleteOne(query)
+  res.send(result)
+})
+
       
 
   app.get('/tasks' ,async(req , res) =>{
